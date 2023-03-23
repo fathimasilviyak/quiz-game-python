67529a020fcbcec8609ae795df77716c4b646ae0
fathimasilviyak <silviyakvm@gmail.com>
2023-03-23 12:34:36 +0530

    get different questions from trivia database and change current question bank

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from question_brain import QuizBrain
 # Create question bank of question objects
 question_bank = []
 for question in question_data:
-    new_question = Question(question["text"], question["answer"])
+    new_question = Question(question["question"], question["correct_answer"])
     question_bank.append(new_question)
 # print(question_bank[0].answer)
 
