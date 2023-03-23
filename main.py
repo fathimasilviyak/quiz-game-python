7fb0579d6100ef77868d3db890e54acdcf845ce5
fathimasilviyak <silviyakvm@gmail.com>
2023-03-23 11:30:45 +0530

    # Repeat asking questions until all the questions in the question bank are asked

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 
 # Bring up the question and ask user to answer it
-quiz.next_question()
+# quiz.next_question()
 
+# Repeat asking questions until all the questions in the question bank are asked
+while quiz.still_has_question():
+    quiz.next_question()
